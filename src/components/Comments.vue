85514027efb3ddc306a1e642febbdd6ec9c01739
Santosh Managuli <rocksantosh15@gmail.com>
2024-01-14 12:54:24 +0530

    created seperate components for postitems and comments

---
+++ src/components/Comments.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  props: {
+    comments: Array,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h2>Comments</h2>
+    <ul>
+      <li v-for="comment in comments" :key="comment.id">{{ comment.body }}</li>
+    </ul>
+  </div>
+</template>
---
+++ src/components/PostItem.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  props: {
+    post: Object,
+  },
+};
+</script>
+
+<template>
+  <tr>
+    <td>{{ post.id }}</td>
+    <td>{{ post.title }}</td>
+    <td>{{ post.body }}</td>
+    <td>{{ post.userId }}</td>
+    <td><button @click="$emit('fetch-comments', post.id)">Fetch Comments</button></td>
+  </tr>
+</template>
